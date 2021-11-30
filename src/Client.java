2cbec5eaec157fde1d4c7f1103a144eded10a547
Raul Pacheco <eu@raulpacheco.com.br>
2021-11-30 07:56:57 -0300

    first commit

---
+++ src/Client.java
@@ -0,0 +1,5 @@
+public class Client extends Person{
+    public Client(String full_name, String profession, String document_cpf) {
+        super(full_name, profession, document_cpf);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Client client = new Client(
+                "Raul Pacheco Domingos",
+                "Quality Assurance Analyst",
+                "113.588.609-12"
+        );
+
+        Account account = new Account(client);
+
+
+    }
+}
