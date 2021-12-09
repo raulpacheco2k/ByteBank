95e6276d41474a79dad7ea322cde6125b97fdd7b
Raul Pacheco <eu@raulpacheco.com.br>
2021-12-09 13:17:41 -0300

    Torna o Client autenticavel

--- src/Bank/Client.java
+++ src/Bank/Client.java
@@ -1,7 +1,22 @@
 package Bank;
 
-public class Client extends Person {
+import InternalSystem.Authenticable;
+
+public class Client extends Person implements Authenticable {
+
+    private String password;
+
     public Client(String full_name, String profession, String document_cpf) {
         super(full_name, profession, document_cpf);
+    }
+
+    @Override
+    public String getSenha() {
+        return this.password;
+    }
+
+    @Override
+    public void setSenha(String password) {
+        this.password = password;
     }
 }
