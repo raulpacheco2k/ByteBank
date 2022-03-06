4a40382d1411a1a23c065a5c03b649f841a4a415
raulpacheco2k <eu@raulpacheco.com.br>
2022-03-06 19:19:10 -0300

    Implementa novos cenários de teste

--- tests/CurrentAccountTest.java
+++ tests/CurrentAccountTest.java
@@ -2,17 +2,60 @@ import br.com.bytebank.Bank.Models.Client;
 import br.com.bytebank.Bank.Models.CurrentAccount;
 import br.com.bytebank.Bank.Models.Person;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CurrentAccountTest {
 
-    @Test
-    public void ensureThereIsNoFeeDiscount() {
+    CurrentAccount currentAccount;
 
+    @Before
+    public void setUp() {
         Person person = new Person("Matheus", "000.000.000-00");
         Client client = new Client(person, 1212.00, "Atendente");
-        CurrentAccount currentAccount = new CurrentAccount(client);
+        this.currentAccount = new CurrentAccount(client);
+    }
 
-        Assert.assertTrue(currentAccount.getWithdrawalFee() > 1);
+    @Test
+    public void ensureThereIsNoFeeDiscount() {
+        Assert.assertTrue(this.currentAccount.getWithdrawalFee() > 1);
+    }
+
+    @Test
+    public void withdrawHalfOfTheBalance() {
+        double depositAmount = 500;
+        double withdrawalValue = 250;
+        double withdrawalAmountWithFee = withdrawalValue * this.currentAccount.getWithdrawalFee();
+        double finalBalance = depositAmount - withdrawalAmountWithFee;
+
+        this.currentAccount.deposit(depositAmount);
+        this.currentAccount.withdraw(withdrawalValue);
+
+        Assert.assertEquals(this.currentAccount.balance(), finalBalance, 0.0);
+    }
+
+    @Test
+    public void withdrawTheEntireBalance() {
+        double depositAmount = 500;
+        double withdrawalValue = 476.19;
+        double withdrawalAmountWithFee = withdrawalValue * this.currentAccount.getWithdrawalFee();
+        double finalBalance = depositAmount - withdrawalAmountWithFee;
+
+        this.currentAccount.deposit(depositAmount);
+        this.currentAccount.withdraw(withdrawalValue);
+        Assert.assertEquals(this.currentAccount.balance(), finalBalance, 0.0);
+    }
+
+    @Test
+    public void withdrawOverBalance() {
+        this.currentAccount.deposit(500);
+        this.currentAccount.withdraw(500.01);
+        Assert.assertEquals(500, this.currentAccount.balance(), 0.0);
+    }
+
+    @Test
+    public void depositWithNegativeValue() {
+        this.currentAccount.deposit(-1);
+        Assert.assertEquals(0, this.currentAccount.balance(), 0.0);
     }
 }
