ad3853e6312cc1be56aff1ab3fccac62c7a76dd8
raulpacheco2k <eu@raulpacheco.com.br>
2022-03-18 23:32:36 -0300

    Corrige problema de formatação de valores BR-USA

--- src/br/com/bytebank/Bank/Models/TypeCreditCard.java
+++ src/br/com/bytebank/Bank/Models/TypeCreditCard.java
@@ -1,9 +1,9 @@
 package br.com.bytebank.Bank.Models;
 
 public enum TypeCreditCard {
-    Gold(3.000, 1.200, 300.0),
-    Platinum(8.000, 5400, 450.00),
-    Black(20.000, 100000.00,1.000);
+    Gold(3000, 1200, 300),
+    Platinum(8000, 5400, 450),
+    Black(20000, 100000, 1000);
 
     final private double minimumIncome;
     final private double limit;
