a9bbe619febf67a3913e67e0af723f142a11284f
raulpacheco2k <eu@raulpacheco.com.br>
2022-03-18 23:58:26 -0300

    Adiciona testes de transferências bancárias

--- tests/CurrentAccountTest.java
+++ tests/CurrentAccountTest.java
@@ -65,4 +65,58 @@ public class CurrentAccountTest {
 
         Assert.assertEquals(0, this.currentAccount.balance(), 0.0);
     }
+
+    @Test
+    public void transferLessThanTheAccountBalance() {
+        Person person1 = new Person("Maria", "000.000.000-00");
+        Client client1 = new Client(person1, 3212.00, "Programadora");
+        CurrentAccount mariaAccount = new CurrentAccount(client1);
+
+        Person person2 = new Person("Matheus", "000.000.000-01");
+        Client client2 = new Client(person2, 1212.00, "Atendente");
+        CurrentAccount matheusAccount = new CurrentAccount(client2);
+
+        mariaAccount.deposit(100);
+        matheusAccount.deposit(100);
+        mariaAccount.transfer(50, matheusAccount);
+
+        Assert.assertEquals(50, mariaAccount.balance(), 0.0);
+        Assert.assertEquals(150, matheusAccount.balance(), 0.0);
+    }
+
+    @Test
+    public void transferTheEntireAccountBalance() {
+        Person person1 = new Person("Maria", "000.000.000-00");
+        Client client1 = new Client(person1, 3212.00, "Programadora");
+        CurrentAccount mariaAccount = new CurrentAccount(client1);
+
+        Person person2 = new Person("Matheus", "000.000.000-01");
+        Client client2 = new Client(person2, 1212.00, "Atendente");
+        CurrentAccount matheusAccount = new CurrentAccount(client2);
+
+        mariaAccount.deposit(100);
+        matheusAccount.deposit(100);
+        mariaAccount.transfer(100, matheusAccount);
+
+        Assert.assertEquals(0, mariaAccount.balance(), 0.0);
+        Assert.assertEquals(200, matheusAccount.balance(), 0.0);
+    }
+
+    @Test
+    public void transferMoreThanTheAccountBalance() {
+        Person person1 = new Person("Maria", "000.000.000-00");
+        Client client1 = new Client(person1, 3212.00, "Programadora");
+        CurrentAccount mariaAccount = new CurrentAccount(client1);
+
+        Person person2 = new Person("Matheus", "000.000.000-01");
+        Client client2 = new Client(person2, 1212.00, "Atendente");
+        CurrentAccount matheusAccount = new CurrentAccount(client2);
+
+        mariaAccount.deposit(100);
+        matheusAccount.deposit(100);
+        mariaAccount.transfer(101, matheusAccount);
+
+        Assert.assertEquals(100, mariaAccount.balance(), 0.0);
+        Assert.assertEquals(100, matheusAccount.balance(), 0.0);
+    }
 }
