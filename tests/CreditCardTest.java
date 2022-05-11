2f17aa8b90cce099b419d333e6c2f3de5ee72084
raulpacheco2k <eu@raulpacheco.com.br>
2022-05-11 07:45:59 -0300

    Refactoring and solving lint

--- tests/CreditCardTest.java
+++ tests/CreditCardTest.java
@@ -2,35 +2,36 @@ import br.com.bytebank.Bank.Models.Client;
 import br.com.bytebank.Bank.Models.CreditCard;
 import br.com.bytebank.Bank.Models.Person;
 import br.com.bytebank.Bank.Models.TypeCreditCard;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class CreditCardTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreditCardTest {
 
     CreditCard creditCard;
 
-    @Before
-    public void setUp() {
+    @BeforeEach
+    void setUp() {
         Person person = new Person("Matheus da Silva", "123.456.789-12");
         Client client = new Client(person, 1100.00, "Analyst Test");
         this.creditCard = new CreditCard(client, TypeCreditCard.Gold);
     }
 
     @Test
-    public void spendBelowTheLimit() {
+    void spendBelowTheLimit() {
         double creditCardLimite = TypeCreditCard.Gold.getLimit();
         double amountSpent = 900;
         double atualFreeBalance = creditCardLimite - amountSpent;
 
         this.creditCard.spendCredit(amountSpent);
 
-        Assert.assertEquals(this.creditCard.getFreeBalance(), atualFreeBalance, 0.0);
-        Assert.assertEquals(this.creditCard.getInvoice(), amountSpent, 0.0);
+        assertEquals(this.creditCard.getFreeBalance(), atualFreeBalance, 0.0);
+        assertEquals(this.creditCard.getInvoice(), amountSpent, 0.0);
     }
 
     @Test
-    public void spendAllTheLimit() {
+    void spendAllTheLimit() {
         double amountSpent = this.creditCard.getTypeCreditCard().getLimit();
         double expectedFreeBalance = TypeCreditCard.Gold.getLimit() - amountSpent;
 
@@ -39,46 +40,46 @@ public class CreditCardTest {
         double freeBalance = this.creditCard.getFreeBalance();
         double invoice = this.creditCard.getInvoice();
 
-        Assert.assertEquals(freeBalance, expectedFreeBalance, 0.0);
-        Assert.assertEquals(invoice, amountSpent, 0.0);
+        assertEquals(freeBalance, expectedFreeBalance, 0.0);
+        assertEquals(invoice, amountSpent, 0.0);
     }
 
     @Test
-    public void spendOverTheLimit() {
+    void spendOverTheLimit() {
         double amountSpent = 1300;
         double expectedInvoice = 0;
 
         this.creditCard.spendCredit(amountSpent);
 
-        Assert.assertEquals(this.creditCard.getFreeBalance(), this.creditCard.getTypeCreditCard().getLimit(), 0.0);
-        Assert.assertEquals(this.creditCard.getInvoice(), expectedInvoice, 0.0);
+        assertEquals(this.creditCard.getFreeBalance(), this.creditCard.getTypeCreditCard().getLimit(), 0.0);
+        assertEquals(this.creditCard.getInvoice(), expectedInvoice, 0.0);
     }
 
     @Test
-    public void payLessThanTheTotalPayable() {
+    void payLessThanTheTotalPayable() {
         double amountSpent = 200;
         double amountPay = 100;
         double expectedInvoice = 100;
 
         this.creditCard.spendCredit(amountSpent);
 
-        Assert.assertTrue(this.creditCard.payBill(amountPay));
-        Assert.assertEquals(expectedInvoice, this.creditCard.getInvoice(), 0.0);
+        assertTrue(this.creditCard.payBill(amountPay));
+        assertEquals(expectedInvoice, this.creditCard.getInvoice(), 0.0);
     }
 
     @Test
-    public void payTheTotalToPay() {
+    void payTheTotalToPay() {
         this.creditCard.spendCredit(1200);
 
-        Assert.assertTrue(this.creditCard.payBill(1200));
-        Assert.assertEquals(0, this.creditCard.getInvoice(), 0.0);
+        assertTrue(this.creditCard.payBill(1200));
+        assertEquals(0, this.creditCard.getInvoice(), 0.0);
     }
 
     @Test
-    public void payMoreThanTheTotalPayable() {
+    void payMoreThanTheTotalPayable() {
         this.creditCard.spendCredit(1000);
 
-        Assert.assertFalse(this.creditCard.payBill(1001));
-        Assert.assertEquals(1000, this.creditCard.getInvoice(), 0.0);
+        assertFalse(this.creditCard.payBill(1001));
+        assertEquals(1000, this.creditCard.getInvoice(), 0.0);
     }
 }
